eb2c652efb26aa2bbba3fbb3e8c55273dd249341
sidd5sci <sidd5sci@gmail.com>
2021-05-20 04:24:30 +0530

    merge sort implemented

--- src/algorithms/sorting/merge_sort/MergeSort.java
+++ src/algorithms/sorting/merge_sort/MergeSort.java
@@ -1,6 +1,7 @@
 package algorithms.sorting.merge_sort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MergeSort {
@@ -8,6 +9,18 @@ public class MergeSort {
 
     public MergeSort(){
 
+    }
+
+    public List sort1(List<Integer> array){
+
+        if (array.size() == 1) {
+            return array;
+        }
+
+        List<Integer> left = array.subList(0, array.size() / 2);
+        List<Integer> right = array.subList(array.size() / 2, array.size());
+
+        return sort(sort1(left), sort1(right));
     }
 
     public List sort(List<Integer> array1, List<Integer> array2){
@@ -18,25 +31,43 @@ public class MergeSort {
             return array1;
         }
         List<Integer> mergedList = new ArrayList<Integer>();
-        int largeSize = (array1.size() > array2.size())? array1.size():array2.size();
+        Integer array1Index  = 0;
+        Integer array2Index  = 0;
 
-        for(int i = 0; i < largeSize; i++){
-//            if()
+        while( array1Index < array1.size() && array2Index < array2.size()){
+//            System.out.println(array1.get(array1Index) +" ---- "+ array2.get(array2Index));
+            if(array1.get(array1Index) < array2.get(array2Index)){
+                mergedList.add(array1.get(array1Index));
+                array1Index++;
+            }else{
+                mergedList.add(array2.get(array2Index));
+                array2Index++;
+            }
 
         }
-        // loop  till a1.size || a2.size
-            // if a1.get(i) < a2.get(j)
-//                    r.push(a1.get(i))//
-//                i++
-//            else
-//                r.push(a2.get(j))
-//                    j++
-        return null;
+
+        List<Integer> leftRemaining = array1.subList(array1Index, array1.size());
+        List<Integer> rightRemaining = array2.subList(array2Index, array2.size());
+
+        mergedList.addAll(leftRemaining);
+        mergedList.addAll(rightRemaining);
+
+        return mergedList;
     }
 
 
     public static void main(String[] args){
 
+        MergeSort sortService = new MergeSort();
+        // two arrays should be sorted
+//        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(63, 87, 283, 4, 0));
+//        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(99, 2, 1, 5));
+//
+//        System.out.println(sortService.sort(array1,array2));
+
+        // no pre requirements
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0));
+        System.out.println(sortService.sort1(numbers));
 
     }
 
